0f57b010ce5adf607cdb74330d91f8108507c303
Neosmart1 <86513591+Aziozo@users.noreply.github.com>
2023-07-30 14:51:43 +0300

    v1

---
+++ dist/index.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  // Обработчик события для ссылки с якорем
+  $('a[href^="#"]').click(function (e) {
+    e.preventDefault(); // Отменяем стандартное действие ссылки
+    var targetDiv = $($(this).attr('href')); // Получаем целевой div
+    var offset = targetDiv.offset().top; // Получаем его отступ от верха страницы
+
+    // Плавно прокручиваем страницу до целевого div
+    $('html, body').animate(
+      {
+        scrollTop: offset - ($(window).height() - targetDiv.outerHeight()) / 2,
+      },
+      1000
+    );
+  });
+  $('.calc-in-btn').on('click', function () {
+    var calcIn = parseFloat($('.calc-in-input').val());
+    if (isNaN(calcIn) || calcIn < 1) {
+      $('.calc-out-val').text('0');
+    } else {
+      var calcOut = calcIn * 2.2;
+      $('.calc-out-val').text(calcOut.toFixed(0) + '€/мес.');
+    }
+  });
+});
+
+$(document).ready(function() {
+    $('.main-poper>div').fadeOut()
+    const observer = new IntersectionObserver(function(entries) {
+        entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            $(".main-poper div").each(function(index) {
+            const $div = $(this);
+            $div.delay(index * 700).fadeIn(500);
+            });
+            observer.disconnect();
+        }
+        });
+  });
+  observer.observe($(".main-poper")[0]);
+});
+
+$(document).ready(function () {
+  const inputtop = document.querySelector('#topphone');
+  const ititop = window.intlTelInput(inputtop, {
+    initialCountry: 'auto',
+    geoIpLookup: (callback) => {
+      fetch('https://ipapi.co/json')
+        .then((res) => res.json())
+        .then((data) => callback(data.country_code))
+        .catch(() => callback('us'));
+    },
+  });
+});
+$(document).ready(function () {
+  const inputbot = document.querySelector('#botphone');
+  const itibot = window.intlTelInput(inputbot, {
+    initialCountry: 'auto',
+    geoIpLookup: (callback) => {
+      fetch('https://ipapi.co/json')
+        .then((res) => res.json())
+        .then((data) => callback(data.country_code))
+        .catch(() => callback('us'));
+    },
+  });
+});
+
